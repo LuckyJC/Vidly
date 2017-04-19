cdaeaf891e837633f0c8beea58de1b66601ca0f7
LuckyJC <jimmy@jimmycarson.com>
2017-04-19 10:23:11 -0400

    movies dataset fix

--- Vidly/Migrations/201704062054109_AddMoviesDataSet.cs
+++ Vidly/Migrations/201704062054109_AddMoviesDataSet.cs
@@ -1,34 +1,43 @@
 namespace Vidly.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
-    public partial class AddMoviesDataSet : DbMigration
+
+    public partial class AddMovie : DbMigration
     {
         public override void Up()
         {
             CreateTable(
+                "dbo.Movies",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    Name = c.String(nullable: false, maxLength: 255),
+                    GenreId = c.Byte(nullable: false),
+                    DateAdded = c.DateTime(nullable: false),
+                    ReleaseDate = c.DateTime(nullable: false),
+                    NumberInStock = c.Byte(nullable: false),
+                })
+                .PrimaryKey(t => t.Id)
+                .ForeignKey("dbo.Genres", t => t.GenreId, cascadeDelete: true)
+                .Index(t => t.GenreId);
+
+            CreateTable(
                 "dbo.Genres",
                 c => new
-                    {
-                        Id = c.Byte(nullable: false),
-                        Name = c.String(nullable: false, maxLength: 255),
-                    })
+                {
+                    Id = c.Byte(nullable: false),
+                    Name = c.String(nullable: false, maxLength: 255),
+                })
                 .PrimaryKey(t => t.Id);
-            
-            AddColumn("dbo.Movies", "GenreId", c => c.Byte(nullable: false));
-            CreateIndex("dbo.Movies", "GenreId");
-            AddForeignKey("dbo.Movies", "GenreId", "dbo.Genres", "Id", cascadeDelete: true);
-            DropColumn("dbo.Movies", "Genre");
+
         }
-        
+
         public override void Down()
         {
-            AddColumn("dbo.Movies", "Genre", c => c.String(nullable: false));
             DropForeignKey("dbo.Movies", "GenreId", "dbo.Genres");
             DropIndex("dbo.Movies", new[] { "GenreId" });
-            DropColumn("dbo.Movies", "GenreId");
             DropTable("dbo.Genres");
+            DropTable("dbo.Movies");
         }
     }
 }
