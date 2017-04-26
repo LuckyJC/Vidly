5e0dd1919c47d567214bfde1e988cbc46c39fb85
LuckyJC <jimmy@jimmycarson.com>
2017-04-26 09:58:05 -0400

    add facebook auth, update MS Owin Security Facebook 3.1.0, update external login form, appID placeholder

--- Vidly/App_Start/Startup.Auth.cs
+++ Vidly/App_Start/Startup.Auth.cs
@@ -54,9 +54,9 @@ namespace Vidly
             //   consumerKey: "",
             //   consumerSecret: "");
 
-            //app.UseFacebookAuthentication(
-            //   appId: "",
-            //   appSecret: "");
+            app.UseFacebookAuthentication(
+               appId: "AddAppIdHere",
+               appSecret: "AddAppSecretHere");
 
             //app.UseGoogleAuthentication(new GoogleOAuth2AuthenticationOptions()
             //{
