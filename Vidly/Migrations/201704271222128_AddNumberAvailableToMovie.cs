a0ea8e4060abb7d4b10b91cecf96af43439a13b9
LuckyJC <jimmy@jimmycarson.com>
2017-04-27 12:03:39 -0400

    initialize NumberAvailable to NumberInStock for movies

--- Vidly/Migrations/201704271222128_AddNumberAvailableToMovie.cs
+++ Vidly/Migrations/201704271222128_AddNumberAvailableToMovie.cs
@@ -1,15 +1,16 @@
 namespace Vidly.Migrations
 {
-    using System;
     using System.Data.Entity.Migrations;
-    
+
     public partial class AddNumberAvailableToMovie : DbMigration
     {
         public override void Up()
         {
             AddColumn("dbo.Movies", "NumberAvailable", c => c.Int());
+
+            Sql("UPDATE Movies SET NumberAvailable = NumberInStock");
         }
-        
+
         public override void Down()
         {
             DropColumn("dbo.Movies", "NumberAvailable");
